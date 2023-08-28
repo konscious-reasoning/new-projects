929e1a7b71773f1c0c39a9626ed004f1a1f22cfd
kig4eva <kig4eva@penguin>
2023-08-28 09:57:56 -0400

    Bandname.py

---
+++ 1stweekprojects/.ipynb_checkpoints/bandname-checkpoint.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print(" hello welcome and lets get started")
+#2. Ask the user for the city that they grew up in.
+city = input(" what city do you live in")
+#3. Ask the user for the name of a pet.
+pet = input(" what is your pet name")
+#4. Combine the name of their city and pet and show them their band name.
+band_name = pet + city 
+#5. Make sure the input cursor shows on a new line:
+print("well looks like your ideal band name is " + band_name,)
